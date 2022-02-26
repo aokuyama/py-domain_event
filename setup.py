dada1b5afa9bd407d69f438a5cb151a00796f10f
aokuyama <ao980s@gmail.com>
2022-02-26 09:54:40 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+ 
+setuptools.setup(
+    name="domain_event",
+    version="0.0.1",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="simple domain event package",
+    long_description="simple domain event package",
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
