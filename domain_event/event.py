d43b062e12e05a12a0eb9e68c166a2644edc2ce4
aokuyama <ao980s@gmail.com>
2022-03-05 13:38:05 +0900

    add event basis methods

--- domain_event/event.py
+++ domain_event/event.py
@@ -1,2 +1,34 @@
+import unittest
+
+
 class Event:
-    pass
+    def name(self) -> str:
+        return self.__class__.__name__
+
+    def body(self) -> str:
+        return ""
+
+    def overview(self) -> str:
+        return self.name() + " :\n" + self.body()
+
+
+class TestEvent(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.event = Event()
+
+    class EventA(Event):
+        def body(self):
+            return 'event a'
+
+    def test_名前や概要が自動で取得できる(self):
+        self.assertEqual('Event', self.event.name())
+        a = self.EventA()
+        self.assertEqual('EventA', a.name())
+        self.assertEqual("Event :\n", self.event.overview())
+        self.assertEqual("EventA :\nevent a", a.overview())
+
+
+if __name__ == '__main__':
+    unittest.main()
